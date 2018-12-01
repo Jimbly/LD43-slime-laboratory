fa261a3de456184e75c5108a82b9d19e444d14d2
Jimb Esser <wasteland@gmail.com>
2018-12-01 09:55:09 -0800

    Basic pipe flow

--- src/client/app.js
+++ src/client/app.js
@@ -6,7 +6,7 @@ const local_storage = require('./local_storage.js');
 const particle_data = require('./particle_data.js');
 
 const { v4Build } = VMath;
-const { PI } = Math;
+const { min, max, PI } = Math;
 
 local_storage.storage_prefix = 'glovjs-playground';
 window.Z = window.Z || {};
@@ -59,11 +59,19 @@ export function main(canvas) {
     v4Build(0.820, 0.247, 0.541, 1),
     v4Build(0.541, 0.820, 0.247, 1),
     v4Build(0.247, 0.541, 0.820, 1),
+    v4Build(0.820, 0.522, 0.547, 1),
+    v4Build(0.247, 0.820, 0.525, 1),
+    v4Build(0.533, 0.247, 0.820, 1),
+    v4Build(0.290, 0.208, 0.133, 1),
   ];
   const fluid_colors_glow = [
     v4Build(0.933, 0.400, 0.827, 1),
     v4Build(0.831, 0.933, 0.400, 1),
     v4Build(0.400, 0.827, 0.933, 1),
+    v4Build(0.933, 0.827, 0.827, 1),
+    v4Build(0.400, 0.933, 0.827, 1),
+    v4Build(0.827, 0.400, 0.933, 1),
+    v4Build(0.290, 0.208, 0.133, 1),
   ];
 
   // Cache key_codes
@@ -89,9 +97,10 @@ export function main(canvas) {
     };
   }
   function newGame() {
-    rand = random_seed.create();
+    rand = random_seed.create(1234);
     game_state = {
       board: [],
+      sources: [],
     };
     for (let ii = 0; ii < PIPE_DIM; ++ii) {
       let row = [];
@@ -99,6 +108,14 @@ export function main(canvas) {
         row.push(newPipe());
       }
       game_state.board.push(row);
+      if (rand(2)) {
+        game_state.sources.push({
+          type: rand(3),
+          count: 1 + rand(3),
+        });
+      } else {
+        game_state.sources.push(null);
+      }
     }
   }
 
@@ -132,6 +149,8 @@ export function main(canvas) {
     sprites.beaker_empty = createSpriteSimple('beaker-empty', sprite_size, sprite_size * 1.5,
       defaults({ layers: 1 }, params_beaker));
 
+    sprites.meat = createSpriteSimple('meat', sprite_size, sprite_size, params_square);
+
     sprites.game_bg = createSpriteSimple('bg.png', 2560, 1600, {
       width: game_width,
       height: game_height,
@@ -141,9 +160,25 @@ export function main(canvas) {
 
   function drawPipes(dt) {
     let rotateable = true;
-    let { board } = game_state;
+    let { board, sources } = game_state;
     let x0 = 1440;
     let y0 = 160;
+    // Sources
+    for (let ii = 0; ii < sources.length; ++ii) {
+      let s = sources[ii];
+      if (s) {
+        sprites.meat.drawDualTint({
+          x: x0 + sprite_size * ii + sprite_size / 2,
+          y: y0 - sprite_size / 2,
+          z: Z.SPRITES,
+          color: fluid_colors[s.type],
+          color1: fluid_colors_glow[s.type],
+          size: [1, 1],
+        });
+      }
+    }
+
+    // Pipes
     for (let ii = 0; ii < board.length; ++ii) {
       let row = board[ii];
       for (let jj = 0; jj < row.length; ++jj) {
@@ -174,7 +209,25 @@ export function main(canvas) {
         }
         param.x += sprite_size / 2;
         param.y += sprite_size / 2;
-        sprites.pipes[pipe.type].drawDualTint(param);
+        if (pipe.fill[0].uid) {
+          param.color1 = fluid_colors[pipe.fill[0].type];
+          param.color = fluid_colors_glow[pipe.fill[0].type];
+        }
+        if (pipe.type === 'zig' || pipe.type === 'cross') {
+          let replace = (pipe.type === 'zig') ? 'corner' : 'straight';
+          sprites.pipes[replace].drawDualTint(param);
+          param.rotation += (pipe.type === 'zig') ? PI : PI / 2;
+          if (pipe.fill[1].uid) {
+            param.color1 = fluid_colors[pipe.fill[1].type];
+            param.color = fluid_colors_glow[pipe.fill[1].type];
+          } else {
+            param.color1 = color_pipe;
+            param.color = color_higlight;
+          }
+          sprites.pipes[replace].drawDualTint(param);
+        } else {
+          sprites.pipes[pipe.type].drawDualTint(param);
+        }
       }
     }
   }
@@ -207,9 +260,102 @@ export function main(canvas) {
     });
   }
 
+  const TOP = 0;
+  const RIGHT = 1;
+  const BOTTOM = 2;
+  const LEFT = 3;
+  const dx = [0, 1, 0, -1];
+  const dy = [-1, 0, 1, 0];
+
+  const connectivity = {
+    'corner': [[TOP, RIGHT]],
+    'cross': [[TOP, BOTTOM], [LEFT, RIGHT]],
+    'merge': [[TOP, RIGHT, BOTTOM, LEFT]],
+    'straight': [[TOP, BOTTOM]],
+    't': [[TOP, RIGHT, BOTTOM]],
+    'zig': [[TOP, RIGHT], [LEFT, BOTTOM]],
+  };
+
+  function dofill(x, y, from_dir, type, uid) {
+    if (x < 0 || y < 0 || x >= PIPE_DIM || y >= PIPE_DIM) {
+      return;
+    }
+    let pipe = game_state.board[y][x];
+    let con = connectivity[pipe.type];
+    let check = (from_dir - pipe.rot + 4) % 4;
+
+    let fill_to;
+    let subset;
+    for (let ii = 0; ii < con.length; ++ii) {
+      let conset = con[ii];
+      for (let jj = 0; jj < conset.length; ++jj) {
+        if (conset[jj] === check) {
+          fill_to = conset;
+          subset = ii;
+        }
+      }
+    }
+    if (!fill_to) {
+      return;
+    }
+    let fill = pipe.fill[subset];
+    if (fill.uid === uid) {
+      return;
+    }
+    if (fill.uid) {
+      // already has a type
+      if (type !== fill.type) {
+        if (fill.type < 3) {
+          // good mix
+          let a = min(fill.type, type);
+          let b = max(fill.type, type);
+          fill.type = 3 + (b - 1) * 2 - a; // 0 + 1 = 3; 1 + 2 = 4; 2 + 0 = 5
+        } else {
+          if (fill.type === 6 ||
+            type === 0 && (fill.type === 3 || fill.type === 5) ||
+            type === 1 && (fill.type === 3 || fill.type === 4) ||
+            type === 2 && (fill.type === 4 || fill.type === 5)
+          ) {
+            // already has this color, cancel
+            return;
+          }
+          // bad mix
+          fill.type = 6;
+        }
+      }
+    } else {
+      fill.type = type;
+    }
+    fill.uid = uid;
+
+    // recurse!
+    for (let ii = 0; ii < fill_to.length; ++ii) {
+      if (fill_to[ii] !== check) {
+        let dir = (fill_to[ii] + pipe.rot) % 4;
+        dofill(x + dx[dir], y + dy[dir], (dir + 2) % 4, type, uid);
+      }
+    }
+  }
+
+  function calcFlow() {
+    let { board, sources } = game_state;
+    for (let ii = 0; ii < board.length; ++ii) {
+      let row = board[ii];
+      for (let jj = 0; jj < row.length; ++jj) {
+        row[jj].fill = [{ uid: 0 }, { uid: 0 }];
+      }
+    }
+    for (let ii = 0; ii < sources.length; ++ii) {
+      if (sources[ii] && sources[ii].count) {
+        dofill(ii, 0, TOP, sources[ii].type, ii + 1);
+      }
+    }
+  }
+
   function pipes(dt) {
     draw_list.queue(sprites.game_bg, 0, 0, Z.BACKGROUND);
 
+    calcFlow();
     drawPipes(dt);
     drawBeakers(dt);
 
